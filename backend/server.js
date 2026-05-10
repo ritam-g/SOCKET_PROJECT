d385d45434c8b2c69081ae0fca1c111a262df397
ritam-g <ritammaty2006@gmail.com>
2026-05-10 16:59:25 +0530

    Add initial HTML structure, Tailwind CSS styles, and Vite logo SVG
    - Created index.html with basic HTML structure and linked assets.
    - Added Tailwind CSS file for styling.
    - Included Vite logo SVG for branding.

--- backend/server.js
+++ backend/server.js
@@ -2,28 +2,38 @@ import { createServer } from 'node:http';
 import { Server } from 'socket.io';
 import express from 'express';
 import cors from 'cors';
+import path from 'path'
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: [
+            'http://localhost:5173',
+            'https://socketio-chat-frontend.vercel.app',
+
+        ]
     })
 );
 
 const server = createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:5173',
-        methods: ['GET', 'POST'],
-    },
+    origin: [
+        'http://localhost:5173',
+        'https://socketio-chat-frontend.vercel.app',
+    ],
+    methods: ['GET', 'POST'],
 });
 const ROOM = 'group'
 io.on("connection", (socket) => {
     console.log('a user connected', socket.id);
     //event listeners of join room 
-    socket.on('join room',async (name) => {
+    socket.on('join room', async (name) => {
         await socket.join(ROOM);
         console.log(`${name} joined the room`);
         // notificaiton 
@@ -32,22 +42,22 @@ io.on("connection", (socket) => {
         socket.to(ROOM).emit("notification", name);
     });
     // envent for sending message in room 
-    socket.on(`send message`,(msg)=>{
+    socket.on(`send message`, (msg) => {
         // broadcast to all in the room including sender
-         console.log('====================================');
+        console.log('====================================');
         console.log('client send message');
         console.log('====================================');
         io.to(ROOM).emit("new message", msg);
     })
     // now typing event
-    socket.on("typing",(username)=>{
-        console.log(username,'is typing');
-        
+    socket.on("typing", (username) => {
+        console.log(username, 'is typing');
+
         socket.to(ROOM).emit("typing", username);
     })
-    socket.on("stop typing",(username)=>{
-        console.log(username,'stopped typing');
-        
+    socket.on("stop typing", (username) => {
+        console.log(username, 'stopped typing');
+
         socket.to(ROOM).emit("stop typing", username);
     })
     // event listeners of leave room
@@ -57,10 +67,17 @@ io.on("connection", (socket) => {
 
 })
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
-});
 
+/** Serve static files from the React app dist folder */
+app.use(express.static(path.join(__dirname, 'public')));
+/** 
+ * The "catchall" handler: for any request that doesn't 
+ * match one above, send back React's index.html file.
+ */
+app.get('*any', (req, res) => {
+
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 server.listen(3000, () => {
     console.log('Server running at http://localhost:3000');
 });
